2b08ab30814575878e151522991e8df2e884b2b8
听 楼 <2913394404@qq.com>
2021-10-06 15:35:02 +0800

    完成了一个简单的卷积处理模块

--- item/imageOperation.js
+++ item/imageOperation.js
@@ -409,6 +409,16 @@ lt_code.image.convolutionKernel3 = class {
         return lt_code.image.ctx.getImageData(x, y, w, h);
     }
 
+    /**
+     * 放置图片数据
+     * @param {any} x
+     * @param {any} y
+     * @param {ImageData} imageData
+     */
+    setImageData(x, y, imageData) {
+        lt_code.image.ctx.putImageData(imageData, x, y);
+    }
+
     /**使用卷积核对图片进行处理 */
     handleOut() {
         /**有多少行 */
@@ -417,7 +427,7 @@ lt_code.image.convolutionKernel3 = class {
         var list = this.imageData.height;
         this.imageData = this.getImageData();
         var base = this;
-        var ret = new ImageData(line-2, list-2);
+        var ret = new ImageData(line - 2, list - 2);
         var temp = [];
 
         /**
@@ -430,9 +440,20 @@ lt_code.image.convolutionKernel3 = class {
             defautValue = defautValue == NaN || defautValue == null ? 0 : defautValue;
             var ret = [];
             for (var i = x * list * 4 + y * 4; i < x * list * 4 + (y + 1) * 4; i++) {
-                ret.push(this.imageData.data[i]||defautValue);
+                ret.push(this.imageData.data[i] || defautValue);
             }
             return ret;
+        }
+
+        /**
+         * 获取一个像素点的颜色
+         * @param {any} x
+         * @param {any} y
+         */
+        var getColor2 = (x, y) => {
+            x += lt_code.image.constant.startX;
+            y += lt_code.image.constant.startY;
+            return this.getImageData(x, y, 1, 1);
         }
 
         /**
@@ -444,7 +465,7 @@ lt_code.image.convolutionKernel3 = class {
          * @param {any} b
          * @param {any} a
          */
-        var setColor = (x, y, r, g, b, a)=>{
+        var setColor = (x, y, r, g, b, a) => {
             var i = x * ret.height * 4 + y * 4;
             ret.data[i++] = r;
             ret.data[i++] = g;
@@ -452,10 +473,83 @@ lt_code.image.convolutionKernel3 = class {
             ret.data[i] = a;
         }
 
+        /**
+         * 放置一个像素点
+         * @param {any} x
+         * @param {any} y
+         * @param {any} r
+         * @param {any} g
+         * @param {any} b
+         * @param {any} a
+         */
+        var setColor2 = (x, y, r, g, b, a) => {
+            x += lt_code.image.constant.startX;
+            y += lt_code.image.constant.startY + lt_code.image.constant.height / 2;
+            var temp = [r, g, b, a];
+            temp = new Uint8ClampedArray(temp);
+            temp = new ImageData(temp,1,1);
+            lt_code.image.ctx.putImageData(temp,x,y);
+        }
+
         //console.log(getColor(Math.floor(line/2), Math.floor(list/2)));
 
         for (var i = 0; i < line; i++) {
             for (var j = 0; j < list; j++) {
+                var p1 = getColor2(i - 1, j - 1),
+                    p2 = getColor2(i - 1, j),
+                    p3 = getColor2(i - 1, j + 1),
+                    p4 = getColor2(i, j - 1),
+                    p5 = getColor2(i, j),
+                    p6 = getColor2(i, j + 1),
+                    p7 = getColor2(i + 1, j - 1),
+                    p8 = getColor2(i + 1, j),
+                    p9 = getColor2(i + 1, j + 1);
+
+                var r = function () {
+                    var ret = p1.data[0] * base.points[0][0];
+                    ret += p2.data[0] * base.points[0][1];
+                    ret += p3.data[0] * base.points[0][2];
+                    ret += p4.data[0] * base.points[1][0];
+                    ret += p5.data[0] * base.points[1][1];
+                    ret += p6.data[0] * base.points[1][2];
+                    ret += p7.data[0] * base.points[2][0];
+                    ret += p8.data[0] * base.points[2][1];
+                    ret += p9.data[0] * base.points[2][2];
+                    return Math.round(ret / 9);
+                }(), g = function () {
+                    var ret = p1.data[1] * base.points[0][0];
+                    ret += p2.data[1] * base.points[0][1];
+                    ret += p3.data[1] * base.points[0][2];
+                    ret += p4.data[1] * base.points[1][0];
+                    ret += p5.data[1] * base.points[1][1];
+                    ret += p6.data[1] * base.points[1][2];
+                    ret += p7.data[1] * base.points[2][0];
+                    ret += p8.data[1] * base.points[2][1];
+                    ret += p9.data[1] * base.points[2][2];
+                    return Math.round(ret / 9);
+                }(), b = function () {
+                    var ret = p1.data[2] * base.points[0][0];
+                    ret += p2.data[2] * base.points[0][1];
+                    ret += p3.data[2] * base.points[0][2];
+                    ret += p4.data[2] * base.points[1][0];
+                    ret += p5.data[2] * base.points[1][1];
+                    ret += p6.data[2] * base.points[1][2];
+                    ret += p7.data[2] * base.points[2][0];
+                    ret += p8.data[2] * base.points[2][1];
+                    ret += p9.data[2] * base.points[2][2];
+                    return Math.round(ret / 9);
+                }(), a = function () {
+                    var ret = p1.data[3] * base.points[0][0];
+                    ret += p2.data[3] * base.points[0][1];
+                    ret += p3.data[3] * base.points[0][2];
+                    ret += p4.data[3] * base.points[1][0];
+                    ret += p5.data[3] * base.points[1][1];
+                    ret += p6.data[3] * base.points[1][2];
+                    ret += p7.data[3] * base.points[2][0];
+                    ret += p8.data[3] * base.points[2][1];
+                    ret += p9.data[3] * base.points[2][2];
+                    return Math.round(ret / 9);
+                }();
                 //var p1 = getColor(i - 1, j - 1),
                 //    p2 = getColor(i - 1, j),
                 //    p3 = getColor(i - 1, j + 1),
@@ -511,87 +605,30 @@ lt_code.image.convolutionKernel3 = class {
                 //    ret += p9[3] * base.points[2][2];
                 //    return Math.round(ret / 9);
                 //}();
-                //if (i > 0 && j > 0 && i < line - 1&&j<list - 1) {
-                    //var t = getColor(i, j);
-                    //temp.push(t[0], t[1], t[2], t[3]);
-                    var p1 = getColor(i - 1, j - 1),
-                        p2 = getColor(i - 1, j),
-                        p3 = getColor(i - 1, j + 1),
-                        p4 = getColor(i, j - 1),
-                        p5 = getColor(i, j),
-                        p6 = getColor(i, j + 1),
-                        p7 = getColor(i + 1, j - 1),
-                        p8 = getColor(i + 1, j),
-                        p9 = getColor(i + 1, j + 1);
+                //temp.push(r, g, b, a);
+                //temp.push(r, 0, 0, 255);
+                //temp.push(0, g, 0, 255);
+                //temp.push(0, 0, b, 255);
+                //temp.push(0, 0, 0, 255);
+                ////temp.push(0, 0, 0, 0);
 
-                    var r = function () {
-                        var ret = p1[0] * base.points[0][0];
-                        ret += p2[0] * base.points[0][1];
-                        ret += p3[0] * base.points[0][2];
-                        ret += p4[0] * base.points[1][0];
-                        ret += p5[0] * base.points[1][1];
-                        ret += p6[0] * base.points[1][2];
-                        ret += p7[0] * base.points[2][0];
-                        ret += p8[0] * base.points[2][1];
-                        ret += p9[0] * base.points[2][2];
-                        return Math.round(ret / 9);
-                    }(), g = function () {
-                        var ret = p1[1] * base.points[0][0];
-                        ret += p2[1] * base.points[0][1];
-                        ret += p3[1] * base.points[0][2];
-                        ret += p4[1] * base.points[1][0];
-                        ret += p5[1] * base.points[1][1];
-                        ret += p6[1] * base.points[1][2];
-                        ret += p7[1] * base.points[2][0];
-                        ret += p8[1] * base.points[2][1];
-                        ret += p9[1] * base.points[2][2];
-                        return Math.round(ret / 9);
-                    }(), b = function () {
-                        var ret = p1[2] * base.points[0][0];
-                        ret += p2[2] * base.points[0][1];
-                        ret += p3[2] * base.points[0][2];
-                        ret += p4[2] * base.points[1][0];
-                        ret += p5[2] * base.points[1][1];
-                        ret += p6[2] * base.points[1][2];
-                        ret += p7[2] * base.points[2][0];
-                        ret += p8[2] * base.points[2][1];
-                        ret += p9[2] * base.points[2][2];
-                        return Math.round(ret / 9);
-                    }(), a = function () {
-                        var ret = p1[3] * base.points[0][0];
-                        ret += p2[3] * base.points[0][1];
-                        ret += p3[3] * base.points[0][2];
-                        ret += p4[3] * base.points[1][0];
-                        ret += p5[3] * base.points[1][1];
-                        ret += p6[3] * base.points[1][2];
-                        ret += p7[3] * base.points[2][0];
-                        ret += p8[3] * base.points[2][1];
-                        ret += p9[3] * base.points[2][2];
-                        return Math.round(ret / 9);
-                    }();
-                    temp.push(r, g, b, a);
-                    //temp.push(r, 0, 0, 255);
-                    //temp.push(0, g, 0, 255);
-                    //temp.push(0, 0, b, 255);
-                    //temp.push(0, 0, 0, 255);
-                    ////temp.push(0, 0, 0, 0);
-
-                    //var t = getColor(i, j);
-                    //temp.push(t[0], t[1], t[2], t[3]);
+                //var t = getColor(i, j);
+                //temp.push(t[0], t[1], t[2], t[3]);
                 //} else {
-                    //var t = getColor(i, j);
-                    //temp.push(t[0], t[1], t[2], t[3]);
+                //var t = getColor(i, j);
+                //temp.push(t[0], t[1], t[2], t[3]);
                 //    temp.push(0, 0, 0, 0);
                 //}
+                setColor2(i, j, r, g, b, a);
                 //setColor(i, j, t[0], t[1], t[2], t[3]);
                 //temp.push(r, g, b, a);
                 //console.log(r + " " + g + " " + b + " " + a);
                 //break;
             }
         }
-        console.log(temp);
-        ret = new ImageData(new Uint8ClampedArray(temp), line, list);
-        lt_code.image.ctx.putImageData(ret, lt_code.image.constant.startX, lt_code.image.constant.height / 2 + lt_code.image.constant.startY);
+        //console.log(temp);
+        //ret = new ImageData(new Uint8ClampedArray(temp), line, list);
+        //lt_code.image.ctx.putImageData(ret, lt_code.image.constant.startX, lt_code.image.constant.height / 2 + lt_code.image.constant.startY);
     }
 }
 
